8c11b05f8962a892c9a936e714f67e2296e80d4a
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-05-26 03:15:12 +0500

    splash design

--- src/Components/SplashScreen.tsx
+++ src/Components/SplashScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import Logo from '../Images/fram5.png'
 
 export const SplashScreen: React.FunctionComponent = () => 
 {
     return <div style={{ width : '100%', height : '100vh', display : 'grid', placeItems : 'center' }} >
-        <div>
-                <span style={{ fontSize : 20, fontWeight : 'bold' }} >Expense Tracker PWA</span>
+        <div style={{ display : 'flex', flexDirection : 'column', alignItems : 'center', border : '1px solid black' }} >
+                <img alt="LOGO" src={Logo} style={{ width : '50%', height : '50%' }} />
+                <span style={{ fontSize : 24, fontWeight : 'bold', background : 'linear-gradient(0deg, rgba(167,167,167,0.7486344879748774) 12%, rgba(60,174,252,1) 100%)', 
+                WebkitBackgroundClip: 'text', WebkitTextFillColor : 'transparent' }} >Expense Tracker PWA</span>
         </div>
     </div>
 }
