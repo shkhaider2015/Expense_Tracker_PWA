8fb61d3b8b1c5ed755d259ee7d50b03c726fd739
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-05-26 02:44:11 +0500

    sad

--- src/Components/SplashScreen.tsx
+++ src/Components/SplashScreen.tsx
@@ -5,7 +5,7 @@ export const SplashScreen: React.FunctionComponent = () =>
 {
     return <div style={{ width : '100%', height : '100vh', display : 'grid', placeItems : 'center' }} >
         <div>
-                <span>Expense Tracker PWA</span>
+                <span style={{ fontSize : 20, fontWeight : 'bold' }} >Expense Tracker PWA</span>
         </div>
     </div>
 }
