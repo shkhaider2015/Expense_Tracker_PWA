fe153bec3a7c7e5613f4a722e26b6f0802add165
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-05-26 09:09:22 +0500

    jhjh

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import { Grid } from '@material-ui/core';
+import { Grid, createMuiTheme, Theme, ThemeProvider } from '@material-ui/core';
 import './App.css';
 import './Components/style.css'
 import { TransactionProvider } from './Reducers/TransactionContext';
 import { AppOrSplash } from './Components/appORsplash';
 
+const outerTheme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: "#780073",
+    },
+  },
+});
+
 function App() {
 
-  return <TransactionProvider> 
-  <Grid container alignContent="center" justify="center" >
-    <Grid item xs={10} sm={8} md={6} lg={4} xl={4} >
-    
-
-<AppOrSplash />
-  </Grid>
-  </Grid >
-    </TransactionProvider>
+  return <TransactionProvider>
+    <ThemeProvider theme={outerTheme} >
+      <Grid container alignContent="center" justify="center" >
+        <Grid item xs={10} sm={8} md={6} lg={4} xl={4} >
+          <AppOrSplash />
+        </Grid>
+      </Grid >
+    </ThemeProvider>
+  </TransactionProvider>
 
 }
 
