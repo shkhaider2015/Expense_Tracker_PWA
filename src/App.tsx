39b8c1f13406885a485848ed4fbea6fc2cb7575b
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-05-27 03:11:59 +0500

    jhjh

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ function App() {
     <ThemeProvider theme={outerTheme} >
       <Grid container alignItems="center" justify="center" style={{ height : '100vh' }} >
         <Grid item xs={12} sm={8} md={6} lg={4} xl={4} className="gridx" >
-          
           <AppOrSplash />
         </Grid>
       </Grid >
